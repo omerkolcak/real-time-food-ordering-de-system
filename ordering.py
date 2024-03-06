820b6cb402eda00dc0b9ef6186196d979a6a4b1c
omerkolcak <o.faruk.kolcak@gmail.com>
2024-03-06 17:45:15 +0300

    add order list table to dashboard

--- ordering.py
+++ ordering.py
@@ -9,8 +9,6 @@ from datetime import datetime, timedelta
 
 from confluent_kafka import SerializingProducer 
 
-ORDER_ID, ORDER_MENU_ITEM_ID = 0,0
-
 def create_single_order(conn,restaurants,users,date):
     """
     Function to randomly create single order.
@@ -22,13 +20,13 @@ def create_single_order(conn,restaurants,users,date):
     global ORDER_ID
     order, order_items = generate_order(cur,user,restaurant,ORDER_ID,date)
     # insert order into database
-    # conn = insert_orders(conn,cur,order)
+    conn = insert_orders(conn,cur,order)
     # insert order items into database
     for item in order_items:
         global ORDER_MENU_ITEM_ID
         
         item['order_item_id'] = ORDER_MENU_ITEM_ID
-        # conn = insert_order_items(conn,cur,item)
+        conn = insert_order_items(conn,cur,item)
 
         ORDER_MENU_ITEM_ID += 1
     
@@ -45,7 +43,7 @@ def simulate_food_ordering_system(conn,cur,start_date=None,end_date=None):
     assert end_date > start_date
 
     # fetch all the restaurant and users from the database.
-    restaurants = fetch_records_from_database(cur,'restaurants')[:3]
+    restaurants = fetch_records_from_database(cur,'restaurants')
     users = fetch_records_from_database(cur,'users')
 
     current_date = start_date
@@ -67,16 +65,24 @@ def simulate_food_ordering_system(conn,cur,start_date=None,end_date=None):
                 producer.produce('order_items_topic', key=str(item['order_item_id']), value=json.dumps(item))
                 producer.flush()
 
-            time.sleep(1)
+            time.sleep(0.5)
 
         # update current date.
         current_date += timedelta(days=1)
-
 
 if __name__ == "__main__":
     # connect to the postgres database
     conn = psycopg2.connect("host=localhost dbname=food-orders-db user=postgres password=postgres")
     cur = conn.cursor()
+
+    try:
+        ORDER_ID = len(fetch_records_from_database(cur,'orders'))
+    except:
+        ORDER_ID = 0
+    try:
+        ORDER_MENU_ITEM_ID = len(fetch_records_from_database(cur,'order_item'))
+    except:
+        ORDER_MENU_ITEM_ID = 0
 
     start_date, end_date = datetime.strptime(sys.argv[1],'%m/%d/%Y'), datetime.strptime(sys.argv[2], '%m/%d/%Y')
     simulate_food_ordering_system(conn,cur,start_date, end_date)
